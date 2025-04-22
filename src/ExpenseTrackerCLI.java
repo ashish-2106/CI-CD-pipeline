b99072da10acc4e30cd133f5a1dc08db241f1cb7
Ashish Jhajharia <149863167+ashish-2106@users.noreply.github.com>
2025-04-22 13:54:09 +0530

    Update ExpenseTrackerCLI.java

--- src/ExpenseTrackerCLI.java
+++ src/ExpenseTrackerCLI.java
@@ -1,8 +1,8 @@
 public class ExpenseTrackerCLI {
     public static void main(String[] args) {
         ExpenseTracker tracker = new ExpenseTracker();
-        tracker.addExpense("Tea", 5.0);
-        tracker.addExpense("Snacks", 25.0);
+        tracker.addExpense("Tea", 10.0);
+        tracker.addExpense("Snacks", 20.0);
 
         for (Expense e : tracker.getExpenses()) {
             System.out.println(e);
